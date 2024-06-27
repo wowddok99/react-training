5069fdf3d7b2cd40b1cd5ecbdf4efb791f627956
전상은 <wowddok99@gmail.com>
2024-06-27 19:23:55 +0900

    04-03 graphql-mutation-args

--- class/pages/section04/04-02-graphql-mutation-args/index.js
+++ class/pages/section04/04-02-graphql-mutation-args/index.js
@@ -1,8 +1,8 @@
 import { useMutation, gql } from "@apollo/client"
 
 const 나의그래프큐엘셋팅 = gql`
-    mutation{
-        createBoard(writer: "철수", title: "안녕하세요", contents: "반갑습니다"){
+    mutation createBoard($writer: String, $title: String, $contents: String){
+        createBoard(writer: $writer, title: $title, contents: $contents){
             _id
             number
             message
@@ -14,7 +14,13 @@ export default function GraphqlMutationPage(){
     const [나의함수] = useMutation(나의그래프큐엘셋팅);
 
     const onClickSubmit = async () => {
-        const result = await 나의함수();
+        const result = await 나의함수({
+            variables: {
+                writer: "훈이",
+                title: "안녕하세요!!",
+                contents: "반갑습니다"
+            }
+        });
         console.log(result);
     }
     
