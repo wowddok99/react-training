1fcdd611dac7a2258a3f1577afac6e6839a7635d
전상은 <wowddok99@gmail.com>
2024-07-02 16:52:32 +0900

    05-03-static-routing-board-query

--- class/pages/section05/05-03-static-routing-board-query-moved/1/index.js
+++ class/pages/section05/05-03-static-routing-board-query-moved/1/index.js
@@ -13,14 +13,13 @@ const FETCH_BOARD = gql`
 
 export default function StaticRoutingBoardQueryMovedPage(){
     const { data } = useQuery(FETCH_BOARD);
-    console.log(data);
 
     return (
         <div>
             <div>1번 게시글 이동이 완료되었습니다.</div>
-            <div>작성자: {data && data.fetchBoard.writer}</div>
-            <div>제목: {data && data.fetchBoard.title}</div>
-            <div>내용: {data && data.fetchBoard.contents}</div>
+            <div>작성자: {data?.fetchBoard.writer}</div>
+            <div>제목: {data?.fetchBoard.title}</div>
+            <div>내용: {data?.fetchBoard.contents}</div>
         </div>
     )
     }
