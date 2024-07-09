62a54a3bb609d811abe05ed995b8a994735f9dd8
전상은 <wowddok99@gmail.com>
2024-07-09 20:18:39 +0900

    06-01-container-presenter

--- class/pages/section06/06-01-container-presenter/index.js
+++ class/pages/section06/06-01-container-presenter/index.js
@@ -1,54 +1,12 @@
-import { useMutation, gql } from "@apollo/client"
-import { useState } from 'react' 
-
-const 나의그래프큐엘셋팅 = gql`
-    mutation createBoard($writer: String, $title: String, $contents: String){
-        createBoard(writer: $writer, title: $title, contents: $contents){
-            _id
-            number
-            message
-        }
-    }
-`
+import BoardWrite from "../../../src/components/units/board/write/BoardWrite.container"
 
 export default function GraphqlMutationPage(){
-    const [writer, setWriter] = useState();
-    const [title, setTitle] = useState();
-    const [contents, setContents] = useState();
-
-    const [나의함수] = useMutation(나의그래프큐엘셋팅);
-
-    const onClickSubmit = async () => {
-        const result = await 나의함수({
-            variables: {
-                writer: writer, // 우측 변수는 state임
-                title: title,
-                contents: contents 
-            }
-        });
-        console.log(result);
-    }
-
-    const onChangeWriter = (event) => {
-        setWriter(event.target.value);
-    }
-
-    const onChangeTitle = (event) => {
-        setTitle(event.target.value);
-    }
-
-    const onChangeContents = (event) => {
-        setContents(event.target.value);
-    }
     
-    // 한 줄일때는 괄호 불필요
-    return(
-        <div>      
-            작성자: <input type="text" onChange={onChangeWriter} />
-            제목: <input type="title" onChange={onChangeTitle} />
-            내용: <input type="text" onChange={onChangeContents} />
-            <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+    return (
+        <div>
+            <div>########### 여기는 페이지 입니다 ##########</div>
+            <BoardWrite></BoardWrite>
+            <div>########### 여기는 페이지 입니다 ##########</div>
         </div>
-
     ) 
 }
---
+++ class/src/components/units/board/write/BoardWrite.presenter.js
@@ -0,0 +1,18 @@
+import { RedInput, BlueButton } from "./BoardWRite.styles"
+
+export default function BoardWriteUI(props){
+
+    return (
+        <div>
+            <div>@@@@@@@@@@@ 여기는 프레젠터입니다 @@@@@@@@</div>
+                <div>      
+                    작성자: <input type="text" onChange={props.bbb} />
+                    제목: <input type="title" onChange={props.ccc} />
+                    내용: <input type="text" onChange={props.ddd} />
+                    <button onClick={props.aaa}>GRAPHQL-API 요청하기</button>
+                </div>
+            <div>@@@@@@@@@@@ 여기는 프레젠터입니다 @@@@@@@@</div>
+        </div>
+    )
+
+}
