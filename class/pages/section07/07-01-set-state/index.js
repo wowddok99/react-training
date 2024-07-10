ca41397a2683d369fc5a90bdae54ddae25e0ceaf
전상은 <wowddok99@gmail.com>
2024-07-10 12:00:39 +0900

    오타 수정

---
+++ class/pages/section07/07-01-set-state/index.js
@@ -0,0 +1,12 @@
+import BoardWrite from "../../../src/components/units/board/write/BoardWrite.container"
+
+export default function GraphqlMutationPage(){
+    
+    return (
+        <div>
+            <div>########### 여기는 페이지 입니다 ##########</div>
+            <BoardWrite></BoardWrite>
+            <div>########### 여기는 페이지 입니다 ##########</div>
+        </div>
+    ) 
+}
---
+++ class/src/components/units/board/07-write/write/BoardWrite.container.js
@@ -0,0 +1,49 @@
+import { useMutation } from "@apollo/client"
+import { useState } from 'react' 
+import BoardWriteUI from "./BoardWrite.presenter";
+import { 나의그래프큐엘셋팅 } from "./BoardWrite.queries";
+
+export default function BoardWrite(){
+    const [writer, setWriter] = useState();
+    const [title, setTitle] = useState();
+    const [contents, setContents] = useState();
+
+    const [나의함수] = useMutation(나의그래프큐엘셋팅);
+
+    const onClickSubmit = async () => {
+        const result = await 나의함수({
+            variables: {
+                writer: writer, // 우측 변수는 state임
+                title: title,
+                contents: contents 
+            }
+        });
+        console.log(result);
+    }
+
+    const onChangeWriter = (event) => {
+        setWriter(event.target.value);
+    }
+
+    const onChangeTitle = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const onChangeContents = (event) => {
+        setContents(event.target.value);
+    }
+
+    return (
+        <div>
+            <div>&&&&&&&&&&& 여기는 컨테이너 입니다 &&&&&&&&&&&</div>
+            <BoardWriteUI 
+            aaa={onClickSubmit}
+            bbb={onChangeWriter}
+            ccc={onChangeTitle}
+            ddd={onChangeContents}
+            />
+            <div>&&&&&&&&&&& 여기는 컨테이너 입니다 &&&&&&&&&&&</div>
+        </div>
+    )
+
+}
