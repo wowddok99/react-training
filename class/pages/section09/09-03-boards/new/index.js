81a0f0e224f6840abbc29fdc7286425d0b7527bc
전상은 <wowddok99@gmail.com>
2024-07-18 13:37:07 +0900

    09-03-boards 중간 커밋

---
+++ class/pages/section09/09-03-boards/new/index.js
@@ -0,0 +1,11 @@
+import BoardWrite from "../../../../src/components/units/board/09-write/BoardWrite.container"
+
+export default function BoardsNew(){
+    
+    return (
+        <div>
+            <BoardWrite></BoardWrite>
+        </div>
+    ) 
+}
+
