65b88c71cafd802b850134250a7fb389512fe402
전상은 <wowddok99@gmail.com>
2024-08-19 00:17:15 +0900

    13-02-library-star 완료

--- pages/section13/13-02-library-star/index.tsx
+++ pages/section13/13-02-library-star/index.tsx
@@ -20,7 +20,7 @@ export default function App(){
         // onChange의 value는 Rate 컴포넌트에서 넘겨받은 value임.
         // <Rate onChange={(value) => setValue(value)} value={value} /> // 3단계 방식
         
-        // Rate의 onChange는 Html의 onChange가 아님 -> ant-design에서 만든 onChange -> 별점을 클릭할때 호출됨.
+        // Rate의 onChange는 Html의 onChange가 아님 -> ant-design에서 만든 onChange -> 별점을 클릭할때 호출됨
         <Rate onChange={setValue} value={value} /> // 4단계 방식
       );
 }
